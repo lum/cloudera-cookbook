6c9ce5772adc12b02c4164a6f710fec9448a196e
Steve <steve@tempo.ai>
2013-07-29 19:37:50 -0700

    added hadoop_env options

--- attributes/default.rb
+++ attributes/default.rb
@@ -51,6 +51,8 @@ default[:hadoop][:hadoop_policy]['security.resourcelocalizer.protocol.acl'] = "*
 default[:hadoop][:hadoop_policy]['security.job.task.protocol.acl'] = "*"
 default[:hadoop][:hadoop_policy]['security.job.client.protocol.acl'] = "*"
 
+default[:hadoop][:hadoop_env]['JAVA_HOME'] = "/usr/lib/jvm/java-7-oracle/"
+
 default[:hadoop][:namenode_port]          = "54310"
 default[:hadoop][:jobtracker_port]        = "54311"
 
