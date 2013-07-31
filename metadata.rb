56473421d37dfe9cf3bee6afe91aa2465f57f1e9
Steve <steve@tempo.ai>
2013-07-31 15:04:07 -0700

    updated version to 1.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cerson@me.com"
 license          "Apache 2.0"
 description      "Installs and configures cloudera (hadoop/hive)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.2'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 %w{ centos redhat fedora debian ubuntu }.each do |os|
   supports os
